55ad31e416d686f17864b013868d9619f41b12aa
İbrahim Yerebakan <ibrahim-yerebakan@windowslive.com>
2021-05-17 14:46:06 +0300

    cacheable' cache'e taşındı

--- CQRS/Behaviours/CachingProcessor.cs
+++ CQRS/Behaviours/CachingProcessor.cs
@@ -1,11 +1,6 @@
 ﻿using Cache;
-using CQRS.Abstraction;
-using CQRS.Queries;
+using Cache.Abstraction;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
--- Services/Product/Product.Application/Queries/GetProductByIdQuery.cs
+++ Services/Product/Product.Application/Queries/GetProductByIdQuery.cs
@@ -1,15 +1,10 @@
-﻿using CQRS.Abstraction;
+﻿using Cache.Abstraction;
 using CQRS.Queries;
 using Product.Domain.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Product.Application.Queries
 {
-    public class GetProductByIdQuery : IQuery<ProductDto>,ICacheable
+    public class GetProductByIdQuery : IQuery<ProductDto>, ICacheable
     {
         public GetProductByIdQuery(int id)
         {
